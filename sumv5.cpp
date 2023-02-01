bff353a8688d612388e2ae8ef096cae5bc09429d
Natalia Sempere <nataliasemp@example.com>
2023-02-01 20:58:00 +0100

    SumV5

---
+++ sumv5.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int suma(int numero){
+    int suma = 0;
+    suma = numero * (numero+1)/2);
+    return suma;}
+    
+int main(){
+    cout<<"Número insertado: ";cin>>numero;
+    while (numero < 0){
+        cout<<"Introduce un número positivo: ";<<endl;
+        cout<<"Número insertado: ";
+        cin>>numero;}
+    
+    cout<<"El resultado es: "<<suma(numero);
+    return 0;
+}
