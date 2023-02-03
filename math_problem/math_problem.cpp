d0a63adcf09866c5d1bb3e1a2edb7135e292805d
Natalia Sempere <nataliasemp@example.com>
2023-02-03 20:06:45 +0100

    Math problem - versiones

---
+++ math_problem/math_problem.cpp
@@ -0,0 +1,13 @@
+/*
+Calcular la media de una muestra.
+
+Versión 1 - Pedimos la cantidad y los datos de una muestra, inicializamos la media, y por último imprimimos un mensaje de salida.
+
+Versión 2 - Creamos una función para pedir los datos de la muestra.
+
+Versión 3 - Calculamos la suma de los datos, y dividimos la suma entre el número de elementos de la muestra (lo que viene siendo la media), imprimimos por pantalla el resultado.
+
+Versión 4 - Si la cantidad de la muestra introducida es menor o igual a 0, saltará un mensaje de error, y lo volverá a pedir.
+
+     */  
+
---
+++ math_problem/math_problem_v3.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> pedir_datos(int n){
+    vector<int> muestra;
+    for (int i = 0; i < n; i++) {
+        int num;
+        cout << "Introduce el número de la posición " << i << ": "; cin >> num;
+        muestra.push_back(num);
+    }
+    return muestra;
+}
+
+int main() {
+    int n;
+    float sum = 0, media;
+    vector<int> muestra;
+    cout << "Introduce el número total de elementos de la muestra: "; cin >> n;
+    muestra = pedir_datos(n);
+    
+    for (int i = 0; i < n; i++){
+        sum += muestra[i];
+    }
+    
+    media = sum / n;
+    
+    cout<<"La media de la muestra es: "<<media<<endl;
+
+  return 0;
+}
