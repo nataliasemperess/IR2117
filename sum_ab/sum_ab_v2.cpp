44e10f411a4be2b2b8fd5afa20fe28c706947d50
Natalia Sempere <nataliasemp@example.com>
2023-02-03 18:41:08 +0100

    Ejercicios básicos sum_ab

---
+++ sum_ab/sum_ab_v2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    long int a, b, sum=0;
+    cout<<"Introduce a: "; cin >> a;
+    while(a<1){
+        cout<<"a no es positivo. Pruebe otra vez: "; cin>>a;
+    }
+    cout<<"Introduce b: "; cin >> b;
+    
+    for(long int i=a; i<=b; i++){
+        sum+=i;
+    }
+    
+    cout<< "La suma es " << sum << endl;
+    
+    return 0;
+}
