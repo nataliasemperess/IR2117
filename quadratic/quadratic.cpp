af74b9e89ca781b4a0fe9fb36c16c0c656ce615a
nataliasemperess <113502742+nataliasemperess@users.noreply.github.com>
2023-02-07 12:54:19 +0100

    Update quadratic.cpp

--- quadratic/quadratic.cpp
+++ quadratic/quadratic.cpp
@@ -1,13 +1,17 @@
 #include <iostream>
+#include <cmath>
 
 int main(){
-    int a,b,c,x1,x2;
+    
+    double a,b,c,x1,x2;
     std::cout<<"Introduce a : ";std::cin>>a;
     std::cout<<"Introduce b : ";std::cin>>b;
     std::cout<<"Introduce c : ";std::cin>>c;
     
-    x1 = 0;
-    x2 = 0;
+    double discriminant = (b*b) -(4*a*c);
+    
+    x1 = (-b + sqrt(discriminant))/(2*a);
+    x2 = (-b - sqrt(discriminant))/(2*a);
     
     std::cout<< "x1 = "<<x1<<std::endl;
     std::cout<< "x2 = "<<x2<<std::endl;
