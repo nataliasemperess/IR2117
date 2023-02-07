abaa7e11742f1ffb3533218670eeaaaadb800d2e
nataliasemperess <113502742+nataliasemperess@users.noreply.github.com>
2023-02-07 17:21:45 +0100

    Create prueba_suma_ab.cpp
    Versión 1

---
+++ sum_ab_def/prueba_suma_ab.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    long int a, b, sum=0;
+    
+    cout<<"Introduce a: "; cin >> a;
+    cout<<"Introduce b: "; cin >> b;
+    for(long int i=a; i<=b; i++){
+        sum+=i;
+        cout<<"Prueba"
+    }
+    cout<< "La suma es " << sum << endl;
+    
+    return 0;
+}
