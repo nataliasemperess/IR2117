de81aa0d406bbd8814c534bffcaa66f3397e2ced
nataliasemperess <113502742+nataliasemperess@users.noreply.github.com>
2023-02-07 12:49:58 +0100

    Create quadratic.cpp

---
+++ quadratic/quadratic.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main(){
+    int a,b,c,x1,x2;
+    std::cout<<"Introduce a : ";std::cin>>a;
+    std::cout<<"Introduce b : ";std::cin>>b;
+    std::cout<<"Introduce c : ";std::cin>>c;
+    
+    x1 = 0;
+    x2 = 0;
+    
+    std::cout<< "x1 = "<<x1<<std::endl;
+    std::cout<< "x2 = "<<x2<<std::endl;
+}
