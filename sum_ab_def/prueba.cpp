7ef0428751055b458f22d6f48aa738b2bb235084
nataliasemperess <113502742+nataliasemperess@users.noreply.github.com>
2023-02-07 17:31:46 +0100

    Update prueba.cpp
    Versión 4

--- sum_ab_def/prueba.cpp
+++ sum_ab_def/prueba.cpp
@@ -1,23 +1,20 @@
 #include <iostream>
 
-using namespace std;
-
 int main(){
     long int a, b, sum=0;
     
-    cout<<"Introduce a: "; cin >> a;
+    std::cout<<"Introduce a: "; std::cin >> a;
     while(a<1){
-        cout<<"a no es positivo. Pruebe otra vez: "; cin>>a;
+        std::cout<<"a no es positivo. Pruebe otra vez: "; std::cin>>a;
     }
-    cout<<"Introduce b: "; cin >> b;
-    while(b<1){
-        cout<<"b no es positivo. Pruebe otra vez: "; cin>>b;
+    std::cout<<"Introduce b: "; std::cin >> b;
+    while(b<1 || b<=a){
+        std::cout<<"b no es positivo, o bien b < a. Pruebe otra vez: "; std::cin>>b;
     }
     for(long int i=a; i<=b; i++){
         sum+=i;
-        cout<<"Prueba"
     }
-    cout<< "La suma es " << sum << endl;
+    std::cout<< "La suma es " << sum << std::endl;
     
     return 0;
 }
