36b5ce9b59b7f78ac4d009889e12e0755d47046f
nataliasemperess <113502742+nataliasemperess@users.noreply.github.com>
2023-02-07 17:29:57 +0100

    Create prueba_sum_ab.cpp
    Versión 4

---
+++ sum_ab_def/prueba_sum_ab.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main(){
+    long int a, b, sum=0;
+    
+    std::cout<<"Introduce a: ";std::cin >> a;
+    while(a<1){
+        std::cout<<"a no es positivo. Pruebe otra vez: "; std::cin>>a;
+    }
+    std::cout<<"Introduce b: "; std::cin >> b;
+    while(b<1 || b<=a){
+        std::cout<<"b no es positivo, o bien b < a. Pruebe otra vez: "; std::cin>>b;
+    }
+    for(long int i=a; i<=b; i++){
+        sum+=i;
+    }
+    std::cout<< "La suma es " << sum << std::endl;
+    
+    return 0;
+}
+
