61cf0ef7c4cbc1396122bbea6e27597560ad513b
nataliasemperess <113502742+nataliasemperess@users.noreply.github.com>
2023-02-09 11:48:48 +0100

    Update sum.cpp
    Change topic type (version 2)

--- new_ros2_ws/examples/sum/sum.cpp
+++ new_ros2_ws/examples/sum/sum.cpp
@@ -1,8 +1,8 @@
 #include "rclcpp/rclcpp.hpp"
-#include "std_msgs/msg/string.hpp"
+#include "std_msgs/msg/int32.hpp"
 #include <iostream>
 
-void topic_callback(const std_msgs::msg::String::SharedPtr msg)
+void topic_callback(const std_msgs::msg::Int32::SharedPtr msg)
 {
   std::cout << msg->data << std::endl;
 }
@@ -10,8 +10,8 @@ void topic_callback(const std_msgs::msg::String::SharedPtr msg)
 int main(int argc, char * argv[])
 {
   rclcpp::init(argc, argv);
-  auto node = rclcpp::Node::make_shared("subscriber");
-  auto subscription = node->create_subscription<std_msgs::msg::String>("topic", 10, topic_callback);
+  auto node = rclcpp::Node::make_shared("sum");
+  auto subscription = node->create_subscription<std_msgs::msg::Int32>("number", 10, topic_callback);
   rclcpp::spin(node);
   rclcpp::shutdown();
   return 0;
