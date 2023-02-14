e0d523c41633064fb9d6572fa948929cf574a33c
nataliasemperess <113502742+nataliasemperess@users.noreply.github.com>
2023-02-14 11:54:13 +0100

    Versión 2

--- arith_mean/aritm_mean_prog.cpp
+++ arith_mean/aritm_mean_prog.cpp
@@ -2,8 +2,13 @@
 
 int main(int argc, char** argv){
     
-    double m = 0;
-
+    double m = 0, s = 0;
+    int n = arg -1;
+    
+    for (int i = 1; i <= n; i++){
+        s += argv[i];
+    }
+    
     std::cout<<"Arithmetic mean: "<< m << std::endl;
     return 0;
     
