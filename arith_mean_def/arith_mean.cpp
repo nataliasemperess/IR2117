431ff99a2e5aaf70961ec85d0eb01966b001c7a5
nataliasemperess <113502742+nataliasemperess@users.noreply.github.com>
2023-02-14 12:21:42 +0100

    Versión 2

--- arith_mean_def/arith_mean.cpp
+++ arith_mean_def/arith_mean.cpp
@@ -2,7 +2,12 @@
 
 int main(int argc, char** argv){
     
-    double m = 0;
+    double m = 0, s = 0;
+    int n = argc -1;
+    
+    for (int i = 1; i <= n; i++){
+        s += argv[i];
+    }
     
     std::cout<<"Arithmetic mean: "<< m << std::endl;
     return 0;
