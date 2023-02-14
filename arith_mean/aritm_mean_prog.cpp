4e5018af6fdf4e9bc3c7013fbc5edf65ce62f572
nataliasemperess <113502742+nataliasemperess@users.noreply.github.com>
2023-02-14 11:55:09 +0100

    Versión 3

--- arith_mean/aritm_mean_prog.cpp
+++ arith_mean/aritm_mean_prog.cpp
@@ -3,11 +3,12 @@
 int main(int argc, char** argv){
     
     double m = 0, s = 0;
-    int n = arg -1;
+    int n = argc -1;
     
     for (int i = 1; i <= n; i++){
-        s += argv[i];
+        s += atoi(argv[i]); // Atoi convierte un texto a un num
     }
+    m = s/n;
     
     std::cout<<"Arithmetic mean: "<< m << std::endl;
     return 0;
