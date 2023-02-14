e3ae2fa28c6d93660dbee70a604bdd7f220959d4
nataliasemperess <113502742+nataliasemperess@users.noreply.github.com>
2023-02-14 12:23:38 +0100

    Versión 4

--- arith_mean_def/arith_mean.cpp
+++ arith_mean_def/arith_mean.cpp
@@ -3,12 +3,16 @@
 int main(int argc, char** argv){
     
     double m = 0, s = 0;
-    int n = argc -1;
+    int n = 0, element;
     
-    for (int i = 1; i <= n; i++){
-        s += atoi(argv[i]); // Atoi convierte un texto a un num
+    std::cin>>element;
+    
+    while(element > 0){
+        n+=1;
+        s+=element;
+        std::cin>>element;
     }
-    m = s/n
+    m = s/n;
     
     std::cout<<"Arithmetic mean: "<< m << std::endl;
     return 0;
