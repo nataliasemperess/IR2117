590e2a815b8dc66a9c5a2b65747ca5dfbcb35b4f
nataliasemperess <al415537@uji.es>
2023-02-16 12:51:34 +0100

    Add velocity values

--- tb3_ws/src/robot_trajectory/src/square.cpp
+++ tb3_ws/src/robot_trajectory/src/square.cpp
@@ -1,6 +1,6 @@
 #include <chrono>
 #include "rclcpp/rclcpp.hpp"
-#include "std_msgs/msg/string.hpp"
+#include "geometry_msgs/msg/twist.hpp"
 
 using namespace std::chrono_literals;
 
@@ -10,10 +10,11 @@ int main(int argc, char * argv[])
   auto node = rclcpp::Node::make_shared("publisher");
   auto publisher = node->create_publisher<geometry_msgs::msg::Twist>("cmd_vel", 10);
   geometry_msgs::msg::Twist message;
-  auto publish_count = 0;
   rclcpp::WallRate loop_rate(500ms);
 
   while (rclcpp::ok()) {
+    message.linear.x = 1.0;
+    message.angular.z = 1.0;
     publisher->publish(message);
     rclcpp::spin_some(node);
     loop_rate.sleep();
