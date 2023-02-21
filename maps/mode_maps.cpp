d7ecc34414adbe0d39ea6ae6381e545db17c4521
nataliasemperess <113502742+nataliasemperess@users.noreply.github.com>
2023-02-21 11:35:36 +0100

    Versión 2

--- maps/mode_maps.cpp
+++ maps/mode_maps.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 
+/* VERSIÓN 1
 std::vector<int> elements;
 std::cin >> elements;
 std::map<int, int> counts;
@@ -7,7 +8,27 @@ std::map<int, int> counts;
 for (int value : elements){
     counts[value]++;
 }
+*/
 
+// VERSIÓN 2
+
+std::vector<int> elements;
+std::cin >> elements;
+std::map<int, int> counts;
+int main (){
+    int m = 0;
+    int m_count = 0;
+    for (auto pair : counts){
+        int value = pair.first;
+        int count = pair.second;
+        if (count > m_count){
+            m = value;
+            m_count = count;
+        }
+    }
+    std::cout<<"Mode: " << m << std::edl;
+    return 0;
+}
 
 /* EXPLICACIÓN
 Recorremos lista, y apuntamos la frecuencia de cada número
