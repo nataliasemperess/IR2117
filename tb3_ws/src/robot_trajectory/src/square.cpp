bb9a0efd5d96b268211b90a0857224f1f31e2470
nataliasemperess <al415537@uji.es>
2023-02-23 12:39:08 +0100

    Definiendo speed

--- tb3_ws/src/robot_trajectory/src/square.cpp
+++ tb3_ws/src/robot_trajectory/src/square.cpp
@@ -9,23 +9,24 @@ int main(int argc, char * argv[])
   rclcpp::init(argc, argv);
   auto node = rclcpp::Node::make_shared("publisher");
   auto publisher = node->create_publisher<geometry_msgs::msg::Twist>("cmd_vel", 10);
+  node -> declare_parameter("speed", 0.1);
   geometry_msgs::msg::Twist message;
   rclcpp::WallRate loop_rate(10ms);
 
+  double speed = node->get_parameter("speed").get_parameter_value().get<double>();
   for(int j=0; j<4; j++){
-
-	  int i=0, n=1000;
-	  while (rclcpp::ok() && (i<n)){
-	    message.linear.x = 0.1;
-	    message.angular.z = 0;
-	    publisher->publish(message);
-	    rclcpp::spin_some(node);
-	    loop_rate.sleep();
-	    i++;
-	  }
+	int i = 0, n = 1000;
+	while (rclcpp::ok() && (i<n)){
+	   message.linear.x = speed;
+	   message.angular.z = 0.0;
+	   publisher->publish(message);
+	   rclcpp::spin_some(node);
+	   loop_rate.sleep();
+	   i++;
+	 }
 	  
 	  i=0;
-	  n=1650;
+	  n=1575;
 	  while (rclcpp::ok() && (i<n)){
 	    message.linear.x = 0;
 	    message.angular.z = 0.1;
