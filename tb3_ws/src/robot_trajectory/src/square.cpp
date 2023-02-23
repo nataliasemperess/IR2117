8a0b9704d9b7e6a9e41da475d39c419d85cebcf7
nataliasemperess <al415537@uji.es>
2023-02-23 12:25:30 +0100

    Update square.cpp

--- tb3_ws/src/robot_trajectory/src/square.cpp
+++ tb3_ws/src/robot_trajectory/src/square.cpp
@@ -10,38 +10,31 @@ int main(int argc, char * argv[])
   auto node = rclcpp::Node::make_shared("publisher");
   auto publisher = node->create_publisher<geometry_msgs::msg::Twist>("cmd_vel", 10);
   geometry_msgs::msg::Twist message;
-  rclcpp::WallRate loop_rate(500ms);
+  rclcpp::WallRate loop_rate(10ms);
 
   for(int j=0; j<4; j++){
 
-  int i=0,n=1000;
-  while (rclcpp::ok() && (i<n)){
-    message.linear.x = 0.1;
-    message.angular.z = 0;
-    publisher->publish(message);
-    rclcpp::spin_some(node);
-    loop_rate.sleep();
-    i++;
-  }
-  
-  i=0, n=170;
-  while (rclcpp::ok() && (i<n)){
-    message.linear.x = 0;
-    message.angular.z = 0.1;
-    publisher->publish(message);
-    rclcpp::spin_some(node);
-    loop_rate.sleep();
-    i++;
-  }
-  
-  while (rclcpp::ok()){
-    message.linear.x = 0;
-    message.angular.z = 0;
-    publisher->publish(message);
-    rclcpp::spin_some(node);
-    loop_rate.sleep();
-    i++;
-  }
+	  int i=0, n=1000;
+	  while (rclcpp::ok() && (i<n)){
+	    message.linear.x = 0.1;
+	    message.angular.z = 0;
+	    publisher->publish(message);
+	    rclcpp::spin_some(node);
+	    loop_rate.sleep();
+	    i++;
+	  }
+	  
+	  i=0;
+	  n=1650;
+	  while (rclcpp::ok() && (i<n)){
+	    message.linear.x = 0;
+	    message.angular.z = 0.1;
+	    publisher->publish(message);
+	    rclcpp::spin_some(node);
+	    loop_rate.sleep();
+	    i++;
+	  }
+	  
   }
   
   rclcpp::shutdown();
