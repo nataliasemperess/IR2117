19ef2890c7a2a451c5876dc6bc73ebaa13b8e395
nataliasemperess <113502742+nataliasemperess@users.noreply.github.com>
2023-02-28 12:22:55 +0100

    Versión 4 y 5

--- matrices/SumMatriz.cpp
+++ matrices/SumMatriz.cpp
@@ -25,9 +25,19 @@ int main(){
     for (int i=0; i<rows;i++){
         s(i) = m.row(i).sum();
     }
+    for (int i=0; i<cols;i++){
+        s(i) = m.col(i).sum();
+    }
     std::cout<<"The sum of rows is: "<<s<<std::endl;
     double maxRowSum = s.maxCoeff();
     std::cout<<"The maxium sum of rows is: "<<maxRowSum<<std::endl;
+
+    std::cout<<"The sum of colums is: "<<s<<std::endl;
+    double maxColSum = s.maxCoeff();
+    std::cout<<"The maxium sum of colums is: "<<maxColSum<<std::endl;
+    
+    double maxRowColSum = maxRowSum > maxColSum ? maxRowSum : maxColSum;
+    std::cout<<"The maxium sum of rows or colums is: "<<maxRowColSum<<std::endl;
 }
             
     
