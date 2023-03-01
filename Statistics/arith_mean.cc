cdb5b000645946d261e409735e62f6a25e40e244
nataliasemperess <113502742+nataliasemperess@users.noreply.github.com>
2023-03-01 11:43:49 +0100

    Versión 4
    Introducir números por teclado, y cuando se introduzca un número igual o menor a 0, se para la ejecución y se muestra el resultado de la media

--- Statistics/arith_mean.cc
+++ Statistics/arith_mean.cc
@@ -3,10 +3,13 @@
 int main(int argc,char** argv){
     
     double arith_mean = 0, suma_num = 0;
-    int cantidad_num = argc - 1;
+    int cantidad_num = 0, element;
+    std::cin>>element;
     
-    for (int i = 1; i <= cantidad_num; i++){
-        suma_num += atoi(argv[i]);
+    while (element > 0){
+        cantidad_num += 1;
+        suma_num += element;
+        std::cin >> element;
     }
     arith_mean = suma_num/cantidad_num;
     std::cout<<"Arith_mean: "<<arith_mean<<std::endl;
