acf4fd7e9767deac47b986e9ef5a3188f3752c7b
nataliasemperess <113502742+nataliasemperess@users.noreply.github.com>
2023-03-01 11:54:26 +0100

    Versión 7
    Con vectores

--- Statistics/arith_mean.cc
+++ Statistics/arith_mean.cc
@@ -1,15 +1,20 @@
 #include <iostream>
+#include <vector>
 
 int main(int argc,char** argv){
     
     double arith_mean = 0, suma_num = 0;
     int cantidad_num = 0, element;
+    std::vector<int> elements;
     std::cin>>element;
     
     while (not std::cin.eof()){ // Cuando se aprete CTRL + D se parará la ejecución
+        elements.push_back(element);
         cantidad_num += 1;
-        suma_num += element;
         std::cin >> element;
+    }
+    for (int i = 0; i < cantidad_num; i++){
+        suma_num += elements[i];
     }
     arith_mean = suma_num/cantidad_num;
     std::cout<<"Arith_mean: "<<arith_mean<<std::endl;
