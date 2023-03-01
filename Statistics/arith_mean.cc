ee647c55336c5fa1b078ab6d79a71cfd7b2e4315
nataliasemperess <113502742+nataliasemperess@users.noreply.github.com>
2023-03-01 11:36:51 +0100

    Versión 3
    Convert argument to integer

--- Statistics/arith_mean.cc
+++ Statistics/arith_mean.cc
@@ -6,7 +6,7 @@ int main(int argc,char** argv){
     int cantidad_num = argc - 1;
     
     for (int i = 1; i <= cantidad_num; i++){
-        suma_num += argv[i];
+        suma_num += atoi(argv[i]);
     }
     arith_mean = suma_num/cantidad_num;
     std::cout<<"Arith_mean: "<<arith_mean<<std::endl;
