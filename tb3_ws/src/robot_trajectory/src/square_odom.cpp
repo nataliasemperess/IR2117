0df857d8a3a8e992948f241b76fee907af36bced
nataliasemperess <al415537@uji.es>
2023-03-02 13:05:28 +0100

    Versión 2 square_odom

--- tb3_ws/src/robot_trajectory/src/square_odom.cpp
+++ tb3_ws/src/robot_trajectory/src/square_odom.cpp
@@ -3,17 +3,25 @@
 #include <cmath>
 #include "rclcpp/rclcpp.hpp"
 #include "geometry_msgs/msg/twist.hpp"
+#include "nav_msgs/msg/odometry.hpp"
 
 using namespace std::chrono_literals;
+
+void odom_callback(const nav_msgs::msg::Odometry::SharedPtr msg){
+	std::cout<< msg << std::endl;
+}
 
 int main(int argc, char * argv[])
 {
   rclcpp::init(argc, argv);
   auto node = rclcpp::Node::make_shared("square_odom");
   auto square_odom = node->create_publisher<geometry_msgs::msg::Twist>("cmd_vel", 10);
+  auto subscripcion = node->create_subscription<nav_msgs::msg::Odometry>("odom", 10, odom_callback);
   node -> declare_parameter("linear_speed", 0.1);
   node -> declare_parameter("angular_speed", 3.1416 / 20);
   geometry_msgs::msg::Twist message;
+ 
+  
   rclcpp::WallRate loop_rate(10ms);
 
   double linear_speed = node->get_parameter("linear_speed").get_parameter_value().get<double>();
