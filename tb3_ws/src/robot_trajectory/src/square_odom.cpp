8c93402932de9bfedc75c6eabc7571c1be0129ce
nataliasemperess <al415537@uji.es>
2023-03-02 12:16:29 +0100

    Versión 1 square_odom

---
+++ tb3_ws/src/robot_trajectory/src/square_odom.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <chrono>
+#include <cmath>
+#include "rclcpp/rclcpp.hpp"
+#include "geometry_msgs/msg/twist.hpp"
+
+using namespace std::chrono_literals;
+
+int main(int argc, char * argv[])
+{
+  rclcpp::init(argc, argv);
+  auto node = rclcpp::Node::make_shared("square_odom");
+  auto square_odom = node->create_publisher<geometry_msgs::msg::Twist>("cmd_vel", 10);
+  node -> declare_parameter("linear_speed", 0.1);
+  node -> declare_parameter("angular_speed", 3.1416 / 20);
+  geometry_msgs::msg::Twist message;
+  rclcpp::WallRate loop_rate(10ms);
+
+  double linear_speed = node->get_parameter("linear_speed").get_parameter_value().get<double>();
+  double angular_speed = node->get_parameter("angular_speed").get_parameter_value().get<double>();
+  for(int j=0; j<4; j++){
+	int i = 0, n = 1/(0.001*linear_speed);
+	while (rclcpp::ok() && (i<n)){
+	   message.linear.x = linear_speed;
+	   message.angular.z = 0.0;
+	   square_odom->publish(message);
+	   rclcpp::spin_some(node);
+	   loop_rate.sleep();
+	   i++;
+	 }
+	  
+	  i=0;
+	  n=(M_PI)/(0.001*angular_speed);
+	  while (rclcpp::ok() && (i<n)){
+	    message.linear.x = 0;
+	    message.angular.z = angular_speed;
+	    square_odom->publish(message);
+	    rclcpp::spin_some(node);
+	    loop_rate.sleep();
+	    i++;
+	  }
+	  
+  }
+  
+  rclcpp::shutdown();
+  return 0;
+}
+
