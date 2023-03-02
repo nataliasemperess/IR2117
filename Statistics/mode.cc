6b684ee20dc3099eb697f363df7d6daee5dadefa
nataliasemperess <113502742+nataliasemperess@users.noreply.github.com>
2023-03-02 10:51:29 +0100

    Update mode.cc

--- Statistics/mode.cc
+++ Statistics/mode.cc
@@ -3,7 +3,7 @@
 
 int main(int argc,char** argv){
     
-    double arith_mean = 0, suma_num = 0;
+    double moda = 0, suma_num = 0;
     int cantidad_num = 0, element;
     std::vector<int> elements;
     std::cin>>element;
@@ -13,7 +13,20 @@ int main(int argc,char** argv){
         cantidad_num += 1;
         std::cin >> element;
     }
-    std::cout<<"Mode: "<<arith_mean<<std::endl;
+    int count[cantidad_num], max = 0;
+    for (int j = 0; j < cantidad_num; j++){
+        count[j] = 0;
+        for (int i = 0; i < cantidad_num; i++){
+            if (elements[i] == elements[j]){
+                count[j]++;
+            }
+        }
+        if (count[j] > count[max]){
+            max = j;
+            moda = count[j];
+        }
+    }
+    std::cout<<"Mode: "<<moda<<std::endl;
     return 0;
 }
     
