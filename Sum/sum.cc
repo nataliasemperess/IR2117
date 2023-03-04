723f1a763b58e9165b8c42727be2a1fd42f1fba9
nataliasemperess <113502742+nataliasemperess@users.noreply.github.com>
2023-03-04 13:01:26 +0100

    Versión 1

---
+++ Sum/sum.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main(){
+    int numero;
+    int sum = 0;
+    std::cout<<"Introduce un número: ";
+    std::cin >> numero;
+    std::cout<<"El número introducido es: "<<numero<<std::endl;
+    return 0;
+}
+    
