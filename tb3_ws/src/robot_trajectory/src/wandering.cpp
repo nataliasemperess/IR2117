992e0751910cffa37d030de622f3a66e776ec001
nataliasemperess <al415537@uji.es>
2023-03-23 13:32:56 +0100

    Versión 4 (por acabar)

--- tb3_ws/src/robot_trajectory/src/wandering.cpp
+++ tb3_ws/src/robot_trajectory/src/wandering.cpp
@@ -5,15 +5,27 @@
 #include "sensor_msgs/msg/laser_scan.hpp"
 
 using namespace std::chrono_literals;
+int min_i = 0; min_j= 0;
 
 void wandering_callback(const sensor_msgs::msg::LaserScan::SharedPtr msg){
-
+	
        for (int i = 0; i <= 9; i++){
           double vector = msg -> ranges[i];
-          std::cout<<"Rangos: "<<vector<<std::endl;}
+          int min_i = 0;
+          if (vector < min_i){
+          	min_i = vector;}
+
+       }
+          
        for (int j = 350; j <= 359; j++){
           double vector = msg -> ranges[j];
-          std::cout<<"Rangos: "<<vector<<std::endl;}
+          int min_j = 0;
+          if (vector < min_j){
+          	min_j = vector;
+          }
+   
+       }
+       std::cout<<"El mínimo del vector es: {"<<min_i<<","<<min_j<<"}"<<std::endl;
 
 }
 
