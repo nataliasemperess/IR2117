e3433d6f9917ad9dce876aa4412003e3172188aa
nataliasemperess <al415537@uji.es>
2023-03-28 11:11:26 +0200

    i

--- tb3_ws/src/robot_trajectory/src/square_odom.cpp
+++ tb3_ws/src/robot_trajectory/src/square_odom.cpp
@@ -1,5 +1,5 @@
-#include <iostream>
 #include <chrono>
+#include <iostream>
 #include <cmath>
 #include "rclcpp/rclcpp.hpp"
 #include "geometry_msgs/msg/twist.hpp"
@@ -7,47 +7,39 @@
 
 using namespace std::chrono_literals;
 
-bool ayuda = false;
-double variable_x0, variable_y0, yaw_inicial,total_dif_pos, dif_angulo;
+double distance_x_ini_actual, distance_y_ini_actual, distance_ini_actual, distance_yaw_ini_actual,variable_x_ini,variable_y_ini,yaw_ini;
 
-void odom_callback(const nav_msgs::msg::Odometry::SharedPtr msg){
-	std::cout<< msg << std::endl;
-}
+bool prueba = false;
 
 void topic_callback(const nav_msgs::msg::Odometry::SharedPtr msg){
-  
-  double variable_x = msg -> pose.pose.position.x;
-  double variable_y = msg -> pose.pose.position.y;
-  std::cout<<"x : "<<variable_x<<std::endl;
-  std::cout<<"y : "<<variable_y<<std::endl;
-  
-  
-  
-  double x1 = msg-> pose.pose.orientation.x;
-  double y1 = msg-> pose.pose.orientation.y;
-  double z1 = msg->pose.pose.orientation.z;
-  double w1 = msg-> pose.pose.orientation.w;
 
+    double variable_x = msg -> pose.pose.position.x;
+    double variable_y = msg -> pose.pose.position.y;
+    std::cout<< "x : " << variable_x <<std::endl;
+    std::cout<< "y : " << variable_y <<std::endl;
 
-  double siny_cosp = 2 * (w1 * z1 + x1 * y1);
-  double cosy_cosp = 1 - 2 * (y1 * y1 + z1 * z1);
-  double yaw = atan2(siny_cosp, cosy_cosp);
-  std::cout<<"angle : "<<yaw<<std::endl;
-  
-  if (not ayuda){
-    variable_x0 = msg -> pose.pose.position.x;
-    variable_y0 = msg -> pose.pose.position.y;
-    yaw_inicial = atan2(siny_cosp, cosy_cosp);
-    ayuda = true;
-   }  
-   double dif_posiciones_x = variable_x - variable_x0;
-   double dif_posiciones_y = variable_y - variable_y0;
-   total_dif_pos = sqrt(pow(dif_posiciones_x, 2) + pow(dif_posiciones_y, 2));
-   std::cout<<"La distancia entre la posicion actual y la inicial es : "<<total_dif_pos<<std::endl;	
-   
-   dif_angulo = abs(yaw - yaw_inicial);
-   std::cout<<"La diferencia entre el angulo actual con el inicial es: "<<std::endl;
-	
+    double x1 = msg -> pose.pose.orientation.x;
+    double y1 = msg -> pose.pose.orientation.y;
+    double z1 = msg -> pose.pose.orientation.z;
+    double w1 = msg -> pose.pose.orientation.w;
+
+    double siny_cosp = 2 * (w1*z1+x1*y1);
+    double cosy_cosp = 1-2*(y1*y1+z1*z1);
+    double yaw = atan2(siny_cosp, cosy_cosp);
+    std::cout<<"angle : "<<yaw<<std::endl;
+   	 
+    if (prueba == false){
+   	 variable_x_ini = msg -> pose.pose.position.x;
+   	 variable_y_ini = msg -> pose.pose.position.y;
+   	 yaw_ini = atan2(siny_cosp, cosy_cosp);
+   	 prueba = true;
+    }
+    distance_x_ini_actual = variable_x - variable_x_ini;
+    distance_y_ini_actual = variable_y - variable_y_ini;
+    distance_ini_actual = sqrt(pow(distance_x_ini_actual,2)+ pow(distance_y_ini_actual,2));
+    std::cout<<"The distance between position initial and actual is : "<<distance_ini_actual<<std::endl;
+    distance_yaw_ini_actual = abs(yaw - yaw_ini);
+    std::cout<<"The distance between angle initial and actual is : "<<distance_yaw_ini_actual<<std::endl;    
 }
 
 int main(int argc, char * argv[])
@@ -55,35 +47,43 @@ int main(int argc, char * argv[])
   rclcpp::init(argc, argv);
   auto node = rclcpp::Node::make_shared("square_odom");
   auto square_odom = node->create_publisher<geometry_msgs::msg::Twist>("cmd_vel", 10);
-  auto subscripcion = node->create_subscription<nav_msgs::msg::Odometry>("odom", 10, odom_callback);
-  node -> declare_parameter("linear_speed", 0.1);
-  node -> declare_parameter("angular_speed", 3.1416 / 20);
+  auto subscription = node->create_subscription<nav_msgs::msg::Odometry>("odom", 10, topic_callback);
+  node->declare_parameter("linear_speed", 0.1);
+  node->declare_parameter("angular_speed", 3.1416/20);
   geometry_msgs::msg::Twist message;
- 
-  
   rclcpp::WallRate loop_rate(10ms);
 
   double linear_speed = node->get_parameter("linear_speed").get_parameter_value().get<double>();
   double angular_speed = node->get_parameter("angular_speed").get_parameter_value().get<double>();
   for(int j=0; j<4; j++){
-	while (rclcpp::ok() && (total_dif_pos < 1.0)){
-	   message.linear.x = linear_speed;
-	   message.angular.z = 0.0;
-	   square_odom->publish(message);
-	   rclcpp::spin_some(node);
-	   loop_rate.sleep();
-	 }
-	  while (rclcpp::ok() && (dif_angulo < (M_PI)/2)){
-	    message.linear.x = 0;
-	    message.angular.z = angular_speed;
-	    square_odom->publish(message);
-	    rclcpp::spin_some(node);
-	    loop_rate.sleep();
-	  }
-	  
+      distance_ini_actual = 0.0;
+      while (rclcpp::ok() && (distance_ini_actual<1)) {
+    	message.linear.x = linear_speed;
+    	message.angular.z = 0.0;
+    	square_odom->publish(message);
+    	rclcpp::spin_some(node);
+    	loop_rate.sleep();
+      }
+      
+      while (rclcpp::ok() && (distance_yaw_ini_actual<(M_PI/2))) {
+    	message.linear.x = 0.0;
+    	message.angular.z = angular_speed;
+    	square_odom->publish(message);
+    	rclcpp::spin_some(node);
+    	loop_rate.sleep();
+      }
+      prueba=false;
   }
-  
+  while (rclcpp::ok()) {
+	message.linear.x = 0.0;
+	message.angular.z = 0.0;
+	square_odom->publish(message);
+	rclcpp::spin_some(node);
+	loop_rate.sleep();
+  }
+ 
   rclcpp::shutdown();
   return 0;
+
 }
 
