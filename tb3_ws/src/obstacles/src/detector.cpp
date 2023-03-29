63eed5815ed48dcae98d13dcb9d98a07d850ab5a
nataliasemperess <113502742+nataliasemperess@users.noreply.github.com>
2023-03-29 18:16:56 +0200

    Adding node parameters

--- tb3_ws/src/obstacles/src/detector.cpp
+++ tb3_ws/src/obstacles/src/detector.cpp
@@ -20,6 +20,15 @@ int main(int argc, char * argv[])
   auto suscription = node->create_subscription<sensor_msgs::msg::LaserScan>("scan", 10, callback);
   publisher = node->create_publisher<example_interfaces::msg::Bool>("obstacle", 10);
   
+  node->declare_parameter("obs_angle_min", -M_PI/8);
+  node->declare_parameter("obs_angle_max", M_PI/8);
+  node->declare_parameter("obs_threshold", 1.0);
+  
+  obs_angle_min = node->get_parameter("obs_angle_min").get_parameter_value().get<double>();
+  obs_angle_max = node->get_parameter("obs_angle_max").get_parameter_value().get<double>();
+  obs_threshold = node->get_parameter("obs_threshold").get_parameter_value().get<double>();
+  
+  
   rclcpp::spin(node);
   rclcpp::shutdown();
   return 0;
