ecd8a3e19c205612cb5358aad6f2de0936dd22d4
nataliasemperess <113502742+nataliasemperess@users.noreply.github.com>
2023-03-29 18:09:43 +0200

    Añadiendo el detector_test.launch

---
+++ tb3_ws/src/obstacles/launch/detector_test.launch.py
@@ -0,0 +1,18 @@
+from launch impory LaunchDescription
+from launch_ros.actions import Node
+import launch.actions
+
+def generate_launch_description():
+    return LaunchDescription([
+        launch.actions.DeclareLaunchArgument('bagfile'),
+        launch.actions.ExecuteProcess(
+            cmd=['ros2', 'bag', 'play',
+                 launch.substitutions.LaunchConfiguration('bagfile')],
+            output='screen'
+        ),
+        Node(
+            package='obstables',
+            executable='detector'
+            )
+    ])
+    
