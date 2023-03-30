fffad32b09e00165860eb906e145ec5e463317e5
nataliasemperess <al415537@uji.es>
2023-03-30 12:58:14 +0200

    Versión 0

---
+++ tb3_ws/src/obstacles/src/avoidance.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include "rclcpp/rclcpp.hpp"
+#include "geometry_msgs/msg/twist.hpp"
+
+using namespace std::chrono_literals;
+
+
+int main(int argc, char * argv[])
+{
+  rclcpp::init(argc, argv);
+  
+  auto node = rclcpp::Node::make_shared("avoidance");
+  auto publisher = node->create_publisher<geometry_msgs::msg::Twist>("cmd_vel", 10);
+  geometry_msgs::msg::Twist message;
+  rclcpp::WallRate loop_rate(50ms);
+  
+  while (rclcpp::ok()){
+  publisher->publish(message);
+  rclcpp::spin_some(node);
+  loop_rate.sleep();
+  }
+  rclcpp::shutdown();
+  return 0;
+}
+
