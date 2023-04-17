0073a991bff1293b96118948bb89951a3146d7f4
nataliasemperess <113502742+nataliasemperess@users.noreply.github.com>
2023-04-17 18:09:58 +0200

    Versión 4

--- tb3_ws/src/robot_trajectory/src/wandering.cpp
+++ tb3_ws/src/robot_trajectory/src/wandering.cpp
@@ -3,31 +3,36 @@
 #include "std_msgs/msg/string.hpp"
 #include "geometry_msgs/msg/twist.hpp"
 #include "sensor_msgs/msg/laser_scan.hpp"
+#include <vector>
+#include <Eigen/Dense>
 
 using namespace std::chrono_literals;
 
-int min_i = 0, min_j= 0;
+std::vector<float> vector;
+int min;
 
 void wandering_callback(const sensor_msgs::msg::LaserScan::SharedPtr msg){
 	
-       for (int i = 0; i <= 9; i++){
+    // Versión 3 : Mostrar el valors del vector ranges:
+    for (int i = 0; i <= 9; i++){
           double vector = msg -> ranges[i];
-          int min_i = 0;
-          if (vector < min_i){
-          	min_i = vector;}
-
-       }
-          
-       for (int j = 350; j <= 359; j++){
+          std::cout<<"Rangos: "<<vector<<std::endl;}
+    
+    for (int j = 350; j <= 359; j++){
           double vector = msg -> ranges[j];
-          int min_j = 0;
-          if (vector < min_j){
-          	min_j = vector;
-          }
+          std::cout<<"Rangos: "<<vector<<std::endl;}
+          
+          
+          
+    // Versión 4 : Calcular y mostrar el mínimo del vector:
+    
+    Eigen::Map<Eigen::VectorXf> vector_Eigen(vector.data(), vector.size());
+    
+    min = vector_Eigen.minCoeff();
+    
+    std::cout<<"El mínimo del vector es: "<<min<<std::endl;
+    
    
-       }
-       std::cout<<"El mínimo del vector es: {"<<min_i<<","<<min_j<<"}"<<std::endl;
-
 }
 
 
