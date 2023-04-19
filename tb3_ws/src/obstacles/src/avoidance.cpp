481d11efb775cc0db9bd1849670771bb12decf78
nataliasemperess <113502742+nataliasemperess@users.noreply.github.com>
2023-04-19 18:18:56 +0200

    Versión 2

--- tb3_ws/src/obstacles/src/avoidance.cpp
+++ tb3_ws/src/obstacles/src/avoidance.cpp
@@ -2,16 +2,25 @@
 #include "rclcpp/rclcpp.hpp"
 #include "geometry_msgs/msg/twist.hpp"
 #include "example_interfaces/msg/bool.hpp"
+#include <string.h>
+
 
 using namespace std::chrono_literals;
 
+bool front = false;
+bool left = false;
+bool right = false;
+
 void callback_front(const example_interfaces::msg::Bool::SharedPtr msg){
+    front = msg -> data;
 }
 
 void callback_left(const example_interfaces::msg::Bool::SharedPtr msg){
+    left = msg -> data;
 }
 
 void callback_right(const example_interfaces::msg::Bool::SharedPtr msg){
+    right = msg -> data;
 }
 
 int main(int argc, char * argv[])
@@ -25,7 +34,8 @@ int main(int argc, char * argv[])
   
   auto subs_left = node->create_subscription<example_interfaces::msg::Bool>("/left/obstacle", 10, callback_left);
   
-   auto subs_right = node->create_subscription<example_interfaces::msg::Bool>("/right/obstacle", 10, callback_right);
+  auto subs_right = node->create_subscription<example_interfaces::msg::Bool>("/right/obstacle", 10, callback_right);
+  
   geometry_msgs::msg::Twist message;
   rclcpp::WallRate loop_rate(50ms);
   
