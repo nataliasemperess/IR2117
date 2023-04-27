be6017c0a6629dfa7e8db91acfa57d029af60242
nataliasemperess <>
2023-04-27 13:16:12 +0200

    Versión 2 - Creando el nodo "rings"

--- tb3_ws/src/olympic/src/rings.cpp
+++ tb3_ws/src/olympic/src/rings.cpp
@@ -1,10 +1,26 @@
 #include <cstdio>
+#include <chrono>
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/string.hpp"
 
-int main(int argc, char ** argv)
+using namespace std::chrono_literals;
+
+int main(int argc, char * argv[])
 {
-  (void) argc;
-  (void) argv;
+ rclcpp::init(argc, argv);
+ auto node = rclcpp::Node::make_shared("rings");
+ auto publisher = node->create_publisher<std_msgs::msg::String>("topic", 10);
+ std_msgs::msg::String message;
+ auto publish_count = 0;
+ rclcpp::WallRate loop_rate(500ms);
 
-  printf("hello world olympic package\n");
-  return 0;
+ while (rclcpp::ok()) {
+   message.data = "Hello, world! " + std::to_string(publish_count++);
+   publisher->publish(message);
+   rclcpp::spin_some(node);
+   loop_rate.sleep();
+ }
+ rclcpp::shutdown();
+ return 0;
 }
+
