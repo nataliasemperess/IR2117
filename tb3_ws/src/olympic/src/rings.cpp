bc0b2225d3b5a1d10d422db13ba67630bb7492e0
nataliasemperess <al415537@uji.es>
2023-05-02 12:16:58 +0200

    Update rings.cpp

--- tb3_ws/src/olympic/src/rings.cpp
+++ tb3_ws/src/olympic/src/rings.cpp
@@ -2,6 +2,7 @@
 #include <chrono>
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
+#include "geometry_msgs/msg/twist.hpp"
 
 using namespace std::chrono_literals;
 
@@ -9,13 +10,13 @@ int main(int argc, char * argv[])
 {
  rclcpp::init(argc, argv);
  auto node = rclcpp::Node::make_shared("rings");
- auto publisher = node->create_publisher<std_msgs::msg::String>("/turtle1/cmd_vel", 10);
- std_msgs::msg::String message;
+ auto publisher = node->create_publisher<geometry_msgs::msg::Twist>("/turtle1/cmd_vel", 10);
+ geometry_msgs::msg::Twist message;
  auto publish_count = 0;
  rclcpp::WallRate loop_rate(500ms);
 
  while (rclcpp::ok()) {
-   message.data = "Hello, world! " + std::to_string(publish_count++);
+   message.linear.x = 1.0; 
    publisher->publish(message);
    rclcpp::spin_some(node);
    loop_rate.sleep();
