17ef45559e9d0f4a774ac10d4a43bf782d9689e4
nataliasemperess <al415537@uji.es>
2023-05-02 12:58:12 +0200

    para  hacer cambios

--- tb3_ws/src/olympic/src/rings.cpp
+++ tb3_ws/src/olympic/src/rings.cpp
@@ -6,6 +6,28 @@
 
 using namespace std::chrono_literals;
 
+
+class MinimalParam : public rclcpp::Node
+{
+public:
+  MinimalParam() 
+  : Node("minimal_param_node")
+  {
+    this -> declare_parameter("my_parameter", "world");
+  
+  timer_t = this -> create_wall_timer(
+    1000ms, std::bind(&MinimalParam::timer_callback, this));
+  }
+  
+  void timer_callback(){
+    std::string my_param = this -> get_parameter("my_parameter").as_string();
+    
+    RCLCPP_INFO(this->get_logger(), "Hello ½s!", my_param.c_str();
+    
+    std::vector<rclcpp::Parameter> all_new_parameters{rclcpp::Parameter("my_parameter","world" this -> set_parameters(all_new_parameters);
+    }
+};
+
 int main(int argc, char * argv[])
 {
  rclcpp::init(argc, argv);
