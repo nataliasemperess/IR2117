458e8a204f2fe2da829081af58efae707017b9a7
nataliasemperess <al415537@uji.es>
2023-05-02 12:29:07 +0200

    Versión 4 - Dibujar un circulo

--- tb3_ws/src/olympic/src/rings.cpp
+++ tb3_ws/src/olympic/src/rings.cpp
@@ -9,14 +9,18 @@ using namespace std::chrono_literals;
 int main(int argc, char * argv[])
 {
  rclcpp::init(argc, argv);
+ 
  auto node = rclcpp::Node::make_shared("rings");
  auto publisher = node->create_publisher<geometry_msgs::msg::Twist>("/turtle1/cmd_vel", 10);
+ 
  geometry_msgs::msg::Twist message;
+ 
  auto publish_count = 0;
  rclcpp::WallRate loop_rate(500ms);
-
+ 
  while (rclcpp::ok()) {
    message.linear.x = 1.0; 
+   message.angular.z = 1.0; 
    publisher->publish(message);
    rclcpp::spin_some(node);
    loop_rate.sleep();
