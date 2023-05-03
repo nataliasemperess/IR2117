53a7b94650dc838c04f23925ab33a51fc911619f
nataliasemperess <113502742+nataliasemperess@users.noreply.github.com>
2023-05-03 12:40:42 +0200

    Versión 5

--- tb3_ws/src/olympic/src/rings.cpp
+++ tb3_ws/src/olympic/src/rings.cpp
@@ -7,39 +7,40 @@
 using namespace std::chrono_literals;
 
 
-class MinimalParam : public rclcpp::Node
-{
-public:
-  MinimalParam() 
-  : Node("minimal_param_node")
-  {
-    this -> declare_parameter("my_parameter", "world");
-  
-  timer_t = this -> create_wall_timer(
-    1000ms, std::bind(&MinimalParam::timer_callback, this));
-  }
-  
-  void timer_callback(){
-    std::string my_param = this -> get_parameter("my_parameter").as_string();
-    
-    RCLCPP_INFO(this->get_logger(), "Hello ½s!", my_param.c_str();
-    
-    std::vector<rclcpp::Parameter> all_new_parameters{rclcpp::Parameter("my_parameter","world" this -> set_parameters(all_new_parameters);
-    }
-};
-
 int main(int argc, char * argv[])
 {
- rclcpp::init(argc, argv);
+    rclcpp::init(argc, argv);
+
+    auto node = rclcpp::Node::make_shared("rings");
+    auto publisher = node->create_publisher<geometry_msgs::msg::Twist>("/turtle1/cmd_vel", 10);
+    
+    node -> declare_parameter("radius", 1.0); // Declare parameter
+
+    geometry_msgs::msg::Twist message;
+    rclcpp::WallRate loop_rate(500ms);
+    
+    double radius = node -> get_parameter("radius").get_parameter_value().get<double>();
+    double time_circle = 2 * M_PI * radius ;
+    int n_iterations = static_cast<int>(time_circle / 0.01); 
+
+
+    // Call service SetPen
+    // r -> 0;
+    // g -> 0;
+    // b -> 255;
+    
+    
+    
+    
+    // Call service TeleportAbsolute
+    
+    // x -> 4.0;
+    // y -> 5.5;
+    // theta -> 0;
+    
  
- auto node = rclcpp::Node::make_shared("rings");
- auto publisher = node->create_publisher<geometry_msgs::msg::Twist>("/turtle1/cmd_vel", 10);
+    
  
- this -> declare_parameter("my_parameter", "world");
- geometry_msgs::msg::Twist message;
- 
- auto publish_count = 0;
- rclcpp::WallRate loop_rate(500ms);
  
  while (rclcpp::ok()) {
    message.linear.x = 1.0; 
